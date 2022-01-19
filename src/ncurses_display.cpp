18508b9de6f26b27031ab2848e39d3812c83ae07
Abdulkarim Saddik <abdulkarim.saddik@gmail.com>
2022-01-19 22:00:24 +0100

    add std::async functionality to the function Processor::utilization as it uses sleep function and we can do in this sleep time or in a part of it some other work

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -36,13 +36,16 @@ std::string NCursesDisplay::ProgressBar(float percent) {
 }
 
 void NCursesDisplay::DisplaySystem(System& system, WINDOW* window) {
+  auto processor_ = system.Cpu();
+  std::future<float> ftr = std::async(&Processor::Utilization, processor_);
   int row{0};
   mvwprintw(window, ++row, 2, ("OS: " + system.OperatingSystem()).c_str());
   mvwprintw(window, ++row, 2, ("Kernel: " + system.Kernel()).c_str());
   mvwprintw(window, ++row, 2, "CPU: ");
   wattron(window, COLOR_PAIR(1));
   mvwprintw(window, row, 10, "");
-  wprintw(window, ProgressBar(system.Cpu().Utilization()).c_str());
+  ftr.wait();
+  wprintw(window, ProgressBar(ftr.get()).c_str());
   wattroff(window, COLOR_PAIR(1));
   mvwprintw(window, ++row, 2, "Memory: ");
   wattron(window, COLOR_PAIR(1));
@@ -108,7 +111,7 @@ void NCursesDisplay::Display(System& system, int n) {
   int x_max{getmaxx(stdscr)};
   WINDOW* system_window = newwin(9, x_max - 1, 0, 0);
   WINDOW* process_window = newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
-
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
   while (1) {
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
